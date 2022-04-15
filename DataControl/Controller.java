3566411d36ad9cc40cfae87fd8e1f7d2d6ece4c5
nickadair8 <adair8@purdue.edu>
2022-04-15 10:31:52 -0400

    user login and create user test

--- DataControl/Controller.java
+++ DataControl/Controller.java
@@ -1,5 +1,9 @@
 package DataControl;
 
+import java.util.Iterator;
+import java.util.Set;
+
+import Model.Course;
 import Model.Model;
 import Networking.Callback;
 import Networking.Request;
@@ -11,7 +15,7 @@ public class Controller {
         this.model = model;
     }
 
-    //#region Get/Set
+    // #region Get/Set
     public Model getModel() {
         return this.model;
     }
@@ -19,21 +23,29 @@ public class Controller {
     public void setModel(Model model) {
         this.model = model;
     }
-    //#endregion
+    // #endregion
 
     public synchronized Callback handleRequest(Request request) {
-        switch(request.getRequestType()) {
+        switch (request.getRequestType()) {
             case CREATE_USER:
-                return null;
+           String userName = (String) request.getData().get(0);
+           String password = (String) request.getData().get(1);
+           String userType = (String) request.getData().get(2);
+            
+             return createUser(userName, password, userType);
 
             case LOGIN_USER:
-                return null;
+            userName = (String) request.getData().get(0);
+            password = (String) request.getData().get(1);
+                return loginUser(userName, password);
 
             case DELETE_USER:
                 return null;
 
             case CREATE_COURSE:
-                return new Callback(model, true, "Changed text omg!");
+            String name = (String) request.getData().get(0);
+            String teacher = (String) request.getData().get(1);
+                return createCourse(name, teacher);
 
             case DELETE_COURSE:
                 return null;
@@ -52,9 +64,98 @@ public class Controller {
 
             case TAKE_QUIZ:
                 return null;
-            
+
             default:
                 return null;
         }
+    }
+
+    // must make multiple fails at every place it could fail
+
+    public Callback createCourse(String name, String teacher) {
+        // synchronized (model) {
+
+        for (Course course : model.getCourses()) {
+            if (course.getName().equals(name)) {
+                return new Callback(model, false, "Course already exists");
+            }
+        }
+        model.getCourses().add(new Course(name, teacher));
+        return new Callback(model, true, "success");
+        // }
+    }
+
+    public Callback createUser(String userName, String password, String userType) {
+        // synchronized (model) {
+        // creates variable lists that contains the keys of the hashtables
+        Set<String> studentKeys = model.getStudents().keySet();
+        Iterator<String> studentItr = studentKeys.iterator();
+        Set<String> teacherKeys = model.getTeachers().keySet();
+        Iterator<String> teacherItr = teacherKeys.iterator();
+        // loops through the keys
+        while (studentItr.hasNext() || teacherItr.hasNext()) {
+            //must search thru hashmap for username
+            if (model.getStudents().containsKey(userName)) {
+                return new Callback(model, false, "Student account already exists");
+            } else if (model.getTeachers().containsKey(userName)) {
+                return new Callback(model, false, "Teacher account already exists");
+            }
+        }
+        if (userType.equals("Student")) {
+            model.getStudents().put(userName, password);
+            return new Callback(model, true, "success");
+        } else {
+            model.getTeachers().put(userName, password);
+            return new Callback(model, true, "success");
+        }
+
+        // }
+    }
+
+    public Callback loginUser(String userName, String password) {
+        // creates variable lists that contains the keys of the hashtables
+        // make incorrect user, password, and both
+        Set<String> studentKeys = model.getStudents().keySet();
+        Iterator<String> studentItr = studentKeys.iterator();
+        Set<String> teacherKeys = model.getTeachers().keySet();
+        Iterator<String> teacherItr = teacherKeys.iterator();
+        // loops through the keys
+        while (studentItr.hasNext() || teacherItr.hasNext()) {
+            if (model.getStudents().containsKey(userName)) {
+                return new Callback(model, false, "Student account already exists");
+            } else if (model.getTeachers().containsKey(userName)) {
+                return new Callback(model, false, "Teacher account already exists");
+            }
+        }
+        return new Callback(model, true, "success");
+    }
+
+    public Callback deleteUser(String userName) {
+        // for student must delete from list, the courses, and their quizzes inside of
+        // those courses
+        // for teachers must delete from has and their course
+        // only neg call back is for when user doesn't exist
+
+        // creates variable lists that contains the keys of the hashtables
+        Set<String> studentKeys = model.getStudents().keySet();
+        Iterator<String> studentItr = studentKeys.iterator();
+        Set<String> teacherKeys = model.getTeachers().keySet();
+        Iterator<String> teacherItr = teacherKeys.iterator();
+        // loops through the keys
+        while (studentItr.hasNext() || teacherItr.hasNext()) {
+            if (model.getStudents().equals(userName)) {
+                return new Callback(model, true, "Student deleted");
+            } else if (model.getTeachers().equals(userName)) {
+                model.getTeachers().put(null, null);
+                // iterate through courses and if it matches teacher name delete it
+                for (int i = 0; i < model.getCourses().size(); i++) {
+                    if (model.getCourses().get(i).contains(userName)) {
+                        model.getCourses().get(i).remove(o)
+                    }
+                }
+                return new Callback(model, true, "Teacher deleted");
+            }
+        }
+
     }
 }
