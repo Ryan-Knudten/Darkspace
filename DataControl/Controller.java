86f1faaba8118a7112b2bb0c0f6493dfa5250a93
nickadair8 <adair8@purdue.edu>
2022-04-26 13:25:39 -0400

    commited changes to the branch

--- DataControl/Controller.java
+++ DataControl/Controller.java
@@ -72,7 +72,7 @@ public class Controller {
                 name = (String) request.getData().get(0);
                 courseName = (String) request.getData().get(1);
                 teacher = (String) request.getData().get(2);
-                return deleteQuiz(name, courseName, teacher);
+                return deleteQuiz(name, courseName);
 
             case GRADE_QUIZ:
                 return null;
@@ -133,21 +133,19 @@ public class Controller {
         Iterator<String> studentItr = studentKeys.iterator();
         Set<String> teacherKeys = model.getTeachers().keySet();
         Iterator<String> teacherItr = teacherKeys.iterator();
-        Callback callback = new Callback(model, false, "");
         // loops through the keys
         while (studentItr.hasNext() || teacherItr.hasNext()) {
             if (model.getStudents().containsKey(userName) && model.getStudents().containsValue(password)) {
-                callback = new Callback(model, true, "Login Successful");
+                return new Callback(model, true, "Login Successful");
             } else if (!model.getStudents().containsKey(userName) || !model.getTeachers().containsKey(userName)) {
-                callback = new Callback(model, false, "Incorrect Username");
+                return new Callback(model, false, "Incorrect Username");
             } else if (!model.getStudents().containsValue(password) || !model.getTeachers().containsValue(password)) {
-                callback = new Callback(model, false, "Incorrect Password");
-            } else if (!model.getStudents().containsKey(userName) && !model.getStudents().containsValue(password)
-                    || !model.getTeachers().containsKey(userName) && !model.getTeachers().containsValue(password)) {
-                callback = new Callback(model, false, "Incorrect Username and Password");
+                return new Callback(model, false, "Incorrect Password");
+            } else {
+                return new Callback(model, false, "DELETE THIS");
             }
         }
-        return callback;
+        return new Callback(model, false, "DELETE THIS");
     }
 
     public Callback deleteUser(String userName, String name, String teacher) {
@@ -156,9 +154,6 @@ public class Controller {
         // for teachers must delete from has and their course
         // only neg call back is for when user doesn't exist
 
-        // creates variable lists that contains the keys of the hashtables
-
-        // loops through the keys
         Set<String> studentKeys = model.getStudents().keySet();
         Iterator<String> studentItr = studentKeys.iterator();
         Set<String> teacherKeys = model.getTeachers().keySet();
@@ -170,18 +165,19 @@ public class Controller {
             } else if (model.getTeachers().containsKey(userName)) {
                 model.getTeachers().remove(userName);
             }
-            for (int i = 0; i < model.getCourses().size(); i++) {
-                if (courses.getStudents().get(i).equals(userName)) {
-                    courses.getStudents().remove(i);
-                    if (courses.getQuizzes().get(i).getName().equals(userName)) {
-                        courses.getQuizzes().remove(i);
-                    }
-                    if (model.getCourses().get(i).getName().equals(userName)) {
-                        model.getCourses().remove(i);
-                    }
+        }
+        for (int i = 0; i < model.getCourses().size(); i++) {
+            if (courses.getStudents().get(i).equals(userName)) {
+                courses.getStudents().remove(i);
+                if (courses.getQuizzes().get(i).getName().equals(userName)) {
+                    courses.getQuizzes().remove(i);
+                }
+                if (model.getCourses().get(i).getName().equals(userName)) {
+                    model.getCourses().remove(i);
                 }
             }
         }
+        return new Callback(model, true, "User deleted");
     }
 
     public Callback deleteCourse(String name, String teacher) {
@@ -219,23 +215,45 @@ public class Controller {
                 quiz.setQuestions(questions);
             }
         }
-        // should i make a switch in here for the different options?
         return new Callback(model, true, "Quiz modified");
     }
 
-    public Callback deleteQuiz(String name, String courseName, String teacher) {
-        Course course = new Course(courseName, teacher);
-        for (int i = 0; i < course.getQuizzes().size(); i++) {
-            if (!course.getQuizzes().get(i).getName().equals(name)) {
-                return new Callback(model, false, "Quiz does not exist");
+    public Callback deleteQuiz(String name, String courseName) {
+        // check to see if its already gone
+        // iterate through model.getCourses and look for name
+        for (Course course : model.getCourses()) {
+            if (course.getName().equals(courseName)) {
+                for (Quiz quiz : course.getQuizzes()) {
+                    if (quiz.getName().equals(name)) {
+                        course.getQuizzes().remove(quiz);
+                        return new Callback(model, true, "Course removed");
+                    } else {
+                        return new Callback(model, false, "Quiz did not exist");
+                    }
+                } 
             } else {
-                course.getQuizzes().remove(i);
+                return new Callback(model, false, "Course does not exist");
             }
         }
-        return new Callback(model, true, "Quiz deleted");
+        return new Callback(model, false, "bro what");
+
     }
 
-    public Callback gradeQuiz(String courseName, String studentName, String quizName, Submission submissions) {
-        //oof
-    }
+    // public Callback gradeQuiz(String courseName, String studentName, String
+    // quizName, Submission submission) {
+    // hold off
+    // }
+
+    // take quiz notes
+    // course name, student name, quiz name
+    // look for sub in quiz has table
+    // if you find the student and quiz name that means its taken
+    // need to check if the course and quiz is still available
+    // if all that is fine put the submission in the quiz hash table as the student
+    // name
+
+    // merge main into this branch
+    // accept ryans changes
+    // pick main for launch.json
+    // adress merge conflicts
 }
